a5b54132c5d6729ca45bab50d5a3ebdad4bc5fe5
MarcosRod42 <lopezmikima@gmail.com>
2025-12-05 23:27:07 -0800

    Second Commit
    Added escape and healing(previous commit had attack)

--- GameSim(KnightsVsWizards)-FinalProject/GameSim(KnightsVsWizards)-FinalProject/GameSim(KnightsVsWizards)-FinalProject.cpp
+++ GameSim(KnightsVsWizards)-FinalProject/GameSim(KnightsVsWizards)-FinalProject/GameSim(KnightsVsWizards)-FinalProject.cpp
@@ -24,9 +24,9 @@ int luckRoll() //Your luck, this will generate a random number between 0 to 99, 
 void attack(Character& attacker, Character& defender) // We use void in order to not get a value back
 {
     int luck = luckRoll();// This means that luck will recieve that random number value.
-    if (luck < 20) // Refer to line 18
+    if (luck < 20) // Refer to line 19
     {
-        cout << "Missed! Luck wasn't on your side." << endl;// Refer to line 18 for info on this
+        cout << "Missed! Luck wasn't on your side." << endl;// Refer to line 19 for info on this
     }
     else // This is what happens if you're actually succesful with your attack.
     {
@@ -36,7 +36,33 @@ void attack(Character& attacker, Character& defender) // We use void in order to
     }
 }
 
+void heal(Character& player)
+{
+    int luck = luckRoll();
+    int healAmount = (player.maxHealth / 5) + (luck % 10);// Base heal + luck
+    player.health += healAmount;
+    if (player.health > player.maxHealth)player.health = player.maxHealth;
+    cout << player.type << " heals for " << healAmount << "HP" << endl;
+}
 
+bool flee(Character& player)
+{
+    int luck = luckRoll();
+    if (luck > 70)
+    {
+        cout << player.type << " successfully fled the battle!" << endl;
+        return true;
+    }
+    else
+    {
+        cout << " Failed to flee! You're still in this fight! " << endl;
+        return false;
+    }
+}
+
+
+
+// THis is a test for git
 int main()
 {
     
